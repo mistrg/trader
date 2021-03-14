0ca27f44652604a970699d3dfa71621ce17f6a45
Zdenek Drbalek <zdenek@drbalek.de>
2021-03-14 19:53:58 +0100

    Removed mongo

--- Binance/BinanceLogic.cs
+++ Binance/BinanceLogic.cs
@@ -550,64 +550,64 @@ namespace Trader.Binance
             return result;
 
         }
-        private void ListenToOrderbook(CancellationToken stoppingToken)
-        {
+        // private void ListenToOrderbook(CancellationToken stoppingToken)
+        // {
 
-            foreach (var pair in Pairs)
-            {
+        //     foreach (var pair in Pairs)
+        //     {
 
-                var t = new Task(async () =>
-                {
-                    while (!stoppingToken.IsCancellationRequested)
-                    {
-                        try
-                        {
-                            var res = await httpClient.GetFromJsonAsync<BIResult>(baseUri + "depth?symbol=" + pair);
+        //         var t = new Task(async () =>
+        //         {
+        //             while (!stoppingToken.IsCancellationRequested)
+        //             {
+        //                 try
+        //                 {
+        //                     var res = await httpClient.GetFromJsonAsync<BIResult>(baseUri + "depth?symbol=" + pair);
 
-                            foreach (var x in res.asks)
-                            {
-                                var amount = double.Parse(x[1]);
-                                var price = double.Parse(x[0]);
-                                var dbEntry = InMemDatabase.Instance.Items.SingleOrDefault(p => p.Exch == nameof(Binance) && p.Pair == pair && p.amount == amount && p.askPrice == price);
-                                if (dbEntry == null)
-                                    InMemDatabase.Instance.Items.Add(new DBItem() { Exch = nameof(Binance), Pair = pair, amount = amount, askPrice = price });
-                            }
-
-
-                            foreach (var x in res.bids)
-                            {
-                                var amount = double.Parse(x[1]);
-                                var price = double.Parse(x[0]);
-
-                                var dbEntry = InMemDatabase.Instance.Items.SingleOrDefault(p => p.Exch == nameof(Binance) && p.Pair == pair && p.amount == amount && p.bidPrice == price);
-                                if (dbEntry == null)
-                                    InMemDatabase.Instance.Items.Add(new DBItem() { Exch = nameof(Binance), Pair = pair, amount = amount, bidPrice = price });
-                            }
-
-                            foreach (var w in InMemDatabase.Instance.Items.Where(p => p.Exch == nameof(Binance) && p.Pair == pair))
-                            {
-                                var askItem = res.asks.SingleOrDefault(p => p[1] == w.amount.ToString() && p[0] == w.askPrice.ToString());
-
-                                var bidItem = res.bids.SingleOrDefault(p => p[1] == w.amount.ToString() && p[0] == w.bidPrice.ToString());
-
-                                if (askItem == null && bidItem == null)
-                                    w.EndDate = DateTime.Now;
-
-                            }
-                        }
-                        catch
-                        {
-                        }
+        //                     foreach (var x in res.asks)
+        //                     {
+        //                         var amount = double.Parse(x[1]);
+        //                         var price = double.Parse(x[0]);
+        //                         var dbEntry = InMemDatabase.Instance.Items.SingleOrDefault(p => p.Exch == nameof(Binance) && p.Pair == pair && p.amount == amount && p.askPrice == price);
+        //                         if (dbEntry == null)
+        //                             InMemDatabase.Instance.Items.Add(new DBItem() { Exch = nameof(Binance), Pair = pair, amount = amount, askPrice = price });
+        //                     }
 
 
-                    }
-                }, stoppingToken);
+        //                     foreach (var x in res.bids)
+        //                     {
+        //                         var amount = double.Parse(x[1]);
+        //                         var price = double.Parse(x[0]);
 
-                t.Start();
+        //                         var dbEntry = InMemDatabase.Instance.Items.SingleOrDefault(p => p.Exch == nameof(Binance) && p.Pair == pair && p.amount == amount && p.bidPrice == price);
+        //                         if (dbEntry == null)
+        //                             InMemDatabase.Instance.Items.Add(new DBItem() { Exch = nameof(Binance), Pair = pair, amount = amount, bidPrice = price });
+        //                     }
 
-            }
+        //                     foreach (var w in InMemDatabase.Instance.Items.Where(p => p.Exch == nameof(Binance) && p.Pair == pair))
+        //                     {
+        //                         var askItem = res.asks.SingleOrDefault(p => p[1] == w.amount.ToString() && p[0] == w.askPrice.ToString());
 
-        }
+        //                         var bidItem = res.bids.SingleOrDefault(p => p[1] == w.amount.ToString() && p[0] == w.bidPrice.ToString());
+
+        //                         if (askItem == null && bidItem == null)
+        //                             w.EndDate = DateTime.Now;
+
+        //                     }
+        //                 }
+        //                 catch
+        //                 {
+        //                 }
+
+
+        //             }
+        //         }, stoppingToken);
+
+        //         t.Start();
+
+        //     }
+
+        // }
 
         public double GetTradingTakerFeeRate()
         {
